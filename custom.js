b092fcc98571fa8a40ed24879fff1b89c0e7b4ed
Md. Jawad Jamil <md.jawadjamil@gmail.com>
2021-08-11 10:46:44 +0600

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,159 @@
+let titles = [
+    "শামীম ওসমানের বাবা-মা'র কবরের ওপরে শ্মশানের মাটি! Shamim Osman",
+    "শিক্ষণীয় হাসির শর্টফিল্ম কুদে কালা কুদে কালা -০৩ Thikana TV",
+    "Abu Taha Muhammad Adnan Waz Status Video",
+    "মাহে রমজানের গান রহমের বৃষ্টি জাইমা নূর",
+    "Pobitro Prem পবিত্র প্রেম Akib Ahmed Islamic Natok 2021",
+    "জাইমার কন্ঠে বিবেক জাগ্রত করার গান কেমন মানুষ তুমি Jaima Noor",
+    "ব্রাজিল vs আর্জেন্টিনা Bangla Islamic Natok 2021 II Akib Islamic Tv",
+    "Seafood Halal or Haram Mizanur Rahman Azhari",
+    "শহীদ কারা Who are the martyrs in Islam মিজানুর রহমান আজহারি",
+    "KEPONNATA-2 কৃপণতা Bangla Islamic Natok 2021 Akib Ahmed",
+    "সাইদি সাহেবের ১৯৮০ সালের আগুন ঝরা বক্তব্য। Delwar Hossain Saidi",
+    "শ্রেষ্ঠ বক্তার শ্রেষ্ঠ বক্তব্য শুনুন জীবন বদলে যাবে Dr.shafiqul Islam masud",
+    "জাইমা নূরের কণ্ঠে নতুন গান Machranga Dekhecho ki মাছরাঙ্গ Muchki Hasir",
+    "গেম আসক্তি PUBG FREE FIRE Islamic Natok Akib Ahmed",
+    "২০ থেকে ২৫ হাজারে সেরা ৫ টি স্মার্টফোন - Best Phones in 20k-25k",
+    "শিশুদের কুরবানী ঈদের নতুন নাশীদ ইসমাঈলের কুরবানী Ismailer Kurbani Jaima Saifa",
+]
+
+let channel_list = [
+    "somoy tv",
+    "thinaka tv.press",
+    "Abu Taw haa muha..",
+    "Jaima noor",
+    "Akib islamic tv",
+    "Jaima Noor",
+    "Akib Islamic Tv",
+    "Mizanur Rahman Azhari",
+    "Mizanur Rahman Azhari",
+    "Akib Islamic Tv",
+    "madina tv",
+    "Dr.shafiqul Islam",
+    "Saimum Shilpigos..",
+    "Akib Islamic Tv",
+    "SamZone",
+    "Panvision Tv",
+]
+
+let channel_logo = [
+    "/Channels/somoy tv.jpg",
+    "/Channels/Thikana Tv.press.jpg",
+    "/Channels/Abu Taw Haa Muhammad Adnan.jpg",
+    "/Channels/Jaima noor.jpg",
+    "/Channels/akib.jpg",
+    "/Channels/Jaima noor.jpg",
+    "/Channels/akib.jpg",
+    "/Channels/mizanur rahman azhari.jpg",
+    "/Channels/mizanur rahman azhari.jpg",
+    "/Channels/akib.jpg",
+    "/Channels/Madina tv.jpg",
+    "/Channels/Dr. Shafiqul Islam.jpg",
+    "/Channels/saimum.jpg",
+    "/Channels/akib.jpg",
+    "/Channels/samZone.jpg",
+    "/Channels/Panvision tv.jpg",
+]
+
+
+let views = [
+    "83K",
+    "131K",
+    "1.8 lakh",
+    "3.4M",
+    "229K",
+    "768K",
+    "225K",
+    "7M",
+    "884K",
+    "89.6K",
+    "2.6M",
+    "1.8M",
+    "359K",
+    "173K",
+    "582K",
+    "3.7M",
+]
+
+
+let date = [
+    "1 hour",
+    "3 days",
+    "5 months",
+    "8 years",
+    "1 year",
+    "12 minutes",
+    "5 months",
+    "2 years",
+    "1 week",
+    "4 weeks",
+    "3 months",
+    "22 hours",
+    "8 months",
+    "2 months",
+    "9 months",
+    "7 days",
+]
+document.getElementByTagName
+
+let videos =[
+    "https://drive.google.com/file/d/1Z4LIiQsux5uu1Dhi3Q3ClbjXilSqOFzL/preview",
+    "https://drive.google.com/file/d/1OdX05VtfRFyAr-1pDyqgiSUIyMkVOns0/preview",
+    "https://drive.google.com/file/d/1pWeQ--wEYUHdOvwhsnlrCW0RWj_hADDS/preview",
+    "https://drive.google.com/file/d/1Z2XerGXVELE_IPD-9rRhZwXlCkkYGno3/preview",
+    "https://drive.google.com/file/d/1bbdwLGSKpIHe_CL-HODbQy-MvgNpffHw/preview",
+    "https://drive.google.com/file/d/1VL0NPlDgRjADZs0zWrLotFouNE9z5vpn/preview",
+    "https://drive.google.com/file/d/1D_2402oD4lDQ8-B8SG8M_OgJ_bHYZPsS/preview",
+    "https://drive.google.com/file/d/1YrIJfPperAtum5N3em5XnWPDcLSvaxke/preview",
+    "https://drive.google.com/file/d/1Ck8Sn5zabbYj0GdmWZU4JmFxEGtNE8fl/preview",
+    "https://drive.google.com/file/d/1o2odlDyDBm10-ZZbF6QNc5LguaFjFxSD/preview",
+    "https://drive.google.com/file/d/1d8YWDOFrXyoUw6estGdvgztopb2AE0qt/preview",
+    "https://drive.google.com/file/d/18OrsRLLGUiP3FP8-H7LTrplcm3arpLnk/preview",
+    "https://drive.google.com/file/d/1FzC5_FmEGY_TfwdzxKKM2LLX_DDSvCB_/preview",
+    "https://drive.google.com/file/d/1cFMHkox0ZRKF10xVmoDiASuIJQ1yOjtI/preview",
+    "https://drive.google.com/file/d/1ef9xNHsNPzrb3lhoBc5QG1fJrw4o9zQi/preview",
+    "https://drive.google.com/file/d/16AsMw0gqyJm4rTxUC3IO3YIL5LM-88Ix/preview",
+]
+
+
+
+for (let index = 0; index < 16; index++) {
+    var element = document.querySelector(".content_part")
+
+    element.innerHTML += `
+    
+    <div class='thumb_vid_wrapper'>
+    <div class='thumb_wrapper'>
+        <img class="thumbnail" src="/thumbs_and_videos/Video-${index+1}/thumb.jpg">
+    </div>
+
+    <div class='title_wrapper'>
+        <div class='left_channel'><img class='profile_pic' src='${channel_logo[index]}'></div>
+
+        <div class='right_title'>
+            <div class='right_top_title'> ${titles[index]}</div>
+
+            <div class='right_chnl_title'>
+                <div class='chnl_name'>
+                    <h6> ${channel_list[index]}</h6>
+                    <i class='fas fa-check-circle'></i>
+                </div>
+
+                <div class='right_time_title'><span>${views[index]} views</span> <span class="point"><i class='fas fa-circle'></i></span> <span>${date[index]} ago</span></div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+    `
+}
+
+let like = document.querySelector("#like")
+
+like.addEventListener("click", function() {
+    like.classList.toggle("fas")
+    like.classList.toggle("far")
+});
+
+
